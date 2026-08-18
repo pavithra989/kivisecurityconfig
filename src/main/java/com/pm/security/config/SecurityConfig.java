cc0c00aed958d296913ea27a54daa900c2c2abca
pavithra989 <pavish0905@gmail.com>
2026-08-18 23:40:39 +0530

    addingUSERsManually

--- src/main/java/com/pm/security/config/SecurityConfig.java
+++ src/main/java/com/pm/security/config/SecurityConfig.java
@@ -10,6 +10,10 @@ import org.springframework.security.config.annotation.web.configurers.AuthorizeH
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -45,8 +49,28 @@ public class SecurityConfig {
             }
         }
         http.csrf(csrfCustomizer) */
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService()
+    {
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("kivi")
+                .password("k@123")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user2 = User
+                .withDefaultPasswordEncoder()
+                .username("kavi")
+                .password("k@1234")
+                .roles("User")
+                .build();
 
 
+
+        return new InMemoryUserDetailsManager(user1,user2);
     }
 
 
